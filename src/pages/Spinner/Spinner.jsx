4655f2c656be5d79ecde0802cb966b36f11b63c3
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-11-24 20:01:48 +0530

    Add a sppiner when page will loading this spinner will be shown

--- src/pages/Spinner/Spinner.jsx
+++ src/pages/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="flex h-screen items-center justify-center space-x-2">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-dashed border-gray-900"></div>
+      <div className="h-8 w-8 animate-spin rounded-full border-4 border-dotted border-primary"></div>
     </div>
   );
 };
